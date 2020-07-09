6e365216fcabadbb2ccca74bdcfbcea6dac84ddf
Ankit Gupta <ankit@tpfs.io>
2020-07-09 09:00:02 -0700

    Init cli

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use structopt::StructOpt;
+
+fn main() {
+    let args = CliArgs::from_args();
+    match args {
+        CliArgs::Hello => {
+            println!("Hello");
+        },
+        CliArgs::Goodbye => {
+            println!("Goodbye");
+        }
+    }
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "mycli", rename_all = "kebab-case")]
+pub enum CliArgs {
+    #[structopt(name = "hello", about = "hello subcommand")]
+    Hello,
+    #[structopt(name = "goodbye", about = "goodbye subcommand")]
+    Goodbye,
+}
